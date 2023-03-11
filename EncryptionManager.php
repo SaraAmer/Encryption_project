f5f3ef9ff2b23018168e3d36ee56b382cab5d18e
Sara Amer <saraamersaad8@gmail.com>
2023-03-11 21:27:55 +0200

    feat: decrypt a file

---
+++ EncryptionManager.php
@@ -0,0 +1,49 @@
+<?php
+
+class EncryptionManager
+{
+    public $first_array = [];
+    public $second_array = [];
+    public function __construct($first_array, $second_array)
+    {
+        $this->first_array = $first_array;
+        $this->second_array = $second_array;
+    }
+
+    public function get_corelation(){
+        $n = 26;
+        $multipled_x_y_array = [];
+        foreach ($this->second_array as $key => $second_array_element){
+            $multipled_x_y_array[] = $second_array_element * $this->first_array[$key];
+        }
+        $sum_multiplication_array = round(array_sum($multipled_x_y_array),1);
+        $sum_x = round(array_sum($this->first_array),1);
+        $sum_y = round(array_sum($this->second_array),1);
+        $powered_x = array_map(function ($value) {return $value ** 2;}, $this->first_array);
+        $sum_powered_x = round(array_sum($powered_x),1);
+        $power_sum_x = $sum_x ** 2;
+        $powered_y = array_map(function ($value) {return $value ** 2;}, $this->second_array);
+        $sum_powered_y = round(array_sum($powered_y),1);
+        $power_sum_y = $sum_y ** 2;
+        $corelation_coefficient = ($n * $sum_multiplication_array - $sum_x * $sum_y) / (sqrt($n * $sum_powered_x - $power_sum_x) * sqrt($n * $sum_powered_y - $power_sum_y));
+        return $corelation_coefficient;
+    }
+
+    public function get_key(){
+        $previous_corelation = -50000;
+        $current_corelation = -1;
+        $corelation = - 50000;
+        $index = 0;
+        $flag = true;
+        $corelation_values = [];
+        while ($index < 26){
+            $corelation_values[] = $this->get_corelation();
+            $array_percentege = array_shift($this->second_array);
+            $this->second_array[] = $array_percentege;
+            $index ++;
+        }
+
+        return array_search(max($corelation_values), $corelation_values);
+    }
+
+}
--- decryption.php
+++ decryption.php
@@ -1,4 +1,39 @@
 <?php
+require_once 'EncryptionManager.php';
 $upload_folder = "Uploads/Decryption/" . basename($_FILES["file_for_decryption"]["name"]);
 $file_path = move_uploaded_file($_FILES['file_for_decryption']['tmp_name'], $upload_folder);
 $file_contents = file_get_contents($upload_folder);
+$alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+$alphabets_apperance = [3312, 573, 1568, 1602, 6192, 966, 769, 1869, 2943, 119, 206, 1579, 1500, 2982, 3261, 1074, 116, 2716, 3072, 4358, 1329, 512, 748, 123, 727, 16];
+$file_contents_array = str_split($file_contents);
+$repeating_number = array_fill(0, 26, 0);
+if (!empty($file_contents_array)){
+    foreach ($file_contents_array as $character){
+        $character_index = array_search(strtolower($character), $alphabets);
+        if($character_index){
+            $repeating_number[$character_index] = $repeating_number[$character_index] ? $repeating_number[$character_index] + 1 : 1;
+        }
+    }
+}
+$text_length = strlen(str_replace(' ', '', trim($file_contents)));
+$array_percentege = array_map(function ($value){  global $text_length; return ($value * 100) /  $text_length;}, $repeating_number);
+$encryption_manager = new EncryptionManager($alphabets_apperance, $array_percentege);
+$key_value = $encryption_manager->get_key();
+$result = '';
+$content = str_split($file_contents);
+if(trim($file_contents)){
+    foreach ($content as $key => $value){
+        $character_index = array_search(strtolower($value), $alphabets);
+        if($character_index){
+            $new_index = $character_index - $key_value;
+            $new_index = $new_index < 0 ? $new_index + 26 : $new_index;
+            $result .= $alphabets[$new_index];
+        }else {
+            $result .= $value;
+        }
+
+    }
+}
+unlink($upload_folder);
+
+var_dump($result);
