c26abd921308dc724e10e88357d3eaff83d5f0ff
Sara Amer <saraamersaad8@gmail.com>
2023-03-11 22:03:10 +0200

    refactor: delete unneeded code

--- encrypt.php
+++ encrypt.php
@@ -5,15 +5,11 @@ $alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q
 $new_array = [];
 $file_path = move_uploaded_file($_FILES['file_for_encryption']['tmp_name'], $upload_folder);
 $file_contents = file_get_contents($upload_folder);
-foreach ($alphabets as $key => $value){
-    $new_key = ($key + $key_value) % 26;
-    $new_array[$new_key] = $value;
-}
 $result = '';
 $content = str_split($file_contents);
 if(trim($file_contents)){
     foreach ($content as $key => $value){
-        $new_character_index = array_search(strtolower($value), $new_array);
+        $new_character_index = ((array_search(strtolower($value), $alphabets)) + $key_value) % 26;
         if($new_character_index){
             $result .= $alphabets[$new_character_index];
         }else {
